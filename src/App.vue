96bdc77556b7c11d1be818ddf23779ec9d609f6f
Sergey Koveshnikov <sergey.kove@mail.ru>
2025-04-26 12:15:28 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <ManagerForm />
+</template>
+
+<script setup lang="ts">
+import ManagerForm from './components/ManagerForm.vue'
+</script>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(Quasar, {
+  plugins: {},
+})
+
+app.mount('#app')
